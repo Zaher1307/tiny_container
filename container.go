79661f8f2714c843a72792b3e0dc62f04cacedff
zaher1307 <ahmedzaher1307@gmail.com>
2022-08-14 11:01:25 +0200

    modify some code mistakes and bad behaviors

--- container.go
+++ container.go
@@ -10,35 +10,44 @@ import (
 )
 
 func main() {
-    switch os.Args[1] {
-    case "run":
-        run()
-    case "child":
-        child()
-    default:
-        panic("bad command")
-    }
+	switch os.Args[1] {
+	case "run":
+        err := run()
+		if err != nil {
+            fmt.Fprintf(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case "child":
+        err := child()
+		if err != nil {
+            fmt.Fprintf(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "Invalid arguments")
+		os.Exit(1)
+	}
 }
 
-func run() {
-    fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+func run() error {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
 
-    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |syscall.CLONE_NEWUSER,
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
 		Unshareflags: syscall.CLONE_NEWNS,
 		UidMappings: []syscall.SysProcIDMap{{
 			ContainerID: 0,
-			HostID: syscall.Getuid(),
-			Size: 1,
+			HostID:      syscall.Getuid(),
+			Size:        1,
 		}},
 		GidMappings: []syscall.SysProcIDMap{{
 			ContainerID: 0,
-			HostID: syscall.Getgid(),
-			Size: 1,
+			HostID:      syscall.Getgid(),
+			Size:        1,
 		}},
 		Credential: &syscall.Credential{
 			Uid: uint32(syscall.Getuid()),
@@ -46,41 +55,76 @@ func run() {
 		},
 	}
 
-    must(cmd.Run())
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func child() {
-    fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+func child() error {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
 
-    cmd := exec.Command(os.Args[2], os.Args[3:]...)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-    cg()
+	err := cg()
+	if err != nil {
+		return err
+	}
 
-    must(syscall.Sethostname([]byte("container")))
-    must(syscall.Chroot("./alpine-fs"))
-    must(syscall.Chdir("/"))
-    must(syscall.Mount("proc", "proc", "proc", 0, ""))
-    must(syscall.Mount("dev", "dev", "tmpfs", 0, ""))
+	err = syscall.Sethostname([]byte("container"))
+	if err != nil {
+		return err
+	}
+	err = syscall.Chroot("./root-fs")
+	if err != nil {
+		return err
+	}
+	err = syscall.Chdir("/")
+	if err != nil {
+		return err
+	}
+	err = syscall.Mount("proc", "proc", "proc", 0, "")
+	if err != nil {
+		return err
+	}
+	err = syscall.Mount("dev", "dev", "tmpfs", 0, "")
+	if err != nil {
+		return err
+	}
 
-    must(cmd.Run())
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
 
-    must(syscall.Unmount("proc", 0))
-    must(syscall.Unmount("dev", 0))
+	err = syscall.Unmount("proc", 0)
+	if err != nil {
+		return err
+	}
+	err = syscall.Unmount("dev", 0)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func cg() {
+func cg() error {
 	cgroups := "/sys/fs/cgroup/"
 	pids := path.Join(cgroups, "pids")
 	os.Mkdir(pids, 0755)
-	must(os.WriteFile(path.Join(pids, "pids.max"), []byte("20"), 0700))
-	must(os.WriteFile(path.Join(pids, "cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
-}
-
-func must(err error) {
-    if err != nil {
-        panic(err)
-    }
+	err := os.WriteFile(path.Join(pids, "pids.max"), []byte("20"), 0700)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(path.Join(pids, "cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700)
+	if err != nil {
+		return err
+	}
+	return nil
 }
