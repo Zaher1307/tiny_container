bb9594c9a5a6b63e8118942644d490ba9ca922aa
zaher1307 <ahmedzaher1307@gmail.com>
2022-10-14 18:51:47 +0200

    fix issue in install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget https://busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox
 chmod a+x busybox
-mkdir -p root-fs/bin root-fs/proc root-fs/dev
-./busybox --install root-fs/bin
+mkdir -p root-fs/usr/bin root-fs/proc root-fs/dev
+./busybox --install root-fs/usr/bin
 rm busybox
 go build -o container container.go
